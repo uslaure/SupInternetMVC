834d6657c7e9258b288cc46a901c25316d3c7096
uslaure <laure-ashley.graimont@supinternet.fr>
2015-04-24 16:32:45 +0200

    index

--- web/index.php
+++ web/index.php
@@ -14,16 +14,13 @@
  *
  *
  */
-$routes = ...
 
 //Thaks to p=, find the current route
-$current_route = ...
 
 //ControllerClassName, end name is ...Controller
-$controller_class = ... ;
 
 //ActionName, end name is ...Action
-$action_name = ...;
+/*$action_name = ...;
 
 $controller = new $controller_class();
 
@@ -39,4 +36,35 @@ $response = $controller->$action_name($request);
 /**
  * Use Twig !
  */
+//require $response['view'];
+<?php
+/**
+* This is you FrontController, the only point of access to your webapp
+*/
+use Symfony\Component\Yaml\Yaml;
+require __DIR__ . '/../vendor/autoload.php';
+/**
+* Use Yaml components for load a config routing, $routes is in yaml app/config/routing.yml :
+*
+* Url will be /index.php?p=route_name
+*
+*
+*/
+$routes = Yaml::parse(file_get_contents(__DIR__.'/../app/config/routing.yml'));
+$currentroute = $routes[$_GET['p']]['controller'];
+$routes_array = explode(':',$currentroute);
+//ControllerClassName, end name is ...Controller
+$controller_class = $routes_array[0];
+//ActionName, end name is ...Action
+$action_name = $routes_array[1];
+$controller = new $controller_class();
+//$Request can by an object
+$request['request'] = &$_POST;
+$request['query'] = &$_GET;
+//...
+//$response can be an object
+$response = $controller->$action_name($request);
+/**
+* Use Twig !
+*/
 require $response['view'];
